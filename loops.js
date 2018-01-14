d323aa93823590d239cc5ef4a4cf372e779b1226
Bo Eun Lee <bolee417@gmail.com>
2018-01-14 22:01:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,13 +2,7 @@ var array = [];
 
 function forLoop (array) {
   for (let i = 0;i <= 25; i++) {
-    if (i === 1) {
-      array.push(`I am ${i} strange loop.`);
-    }
-    else{
-    array.push(`I am ${i} strange loops.`);
-  }
-  }
+   array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   return array;
 }
 
